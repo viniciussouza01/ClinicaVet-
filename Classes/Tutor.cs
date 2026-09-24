2772d61950b04b5c9dd007f15ba590fb0b8f5562
viniciussouza01 <viniciusduarteh777@gmail.com>
2026-09-24 09:32:19 -0300

    Update Tutor.cs

--- Classes/Tutor.cs
+++ Classes/Tutor.cs
@@ -5,7 +5,10 @@ using System.Text;
 namespace ClinicaVeterinaria
 {
     public class Tutor : Pessoa 
-    {
+    { public string? nome { get; set; }
+        public string? telefone { get; set; }
+        public string? email { get; set; }
+
         public string? cpf { get; set; }
         List<Animal> ListaAnimais = new List<Animal>();
 
